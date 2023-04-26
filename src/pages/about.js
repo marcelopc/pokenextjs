c0cbb96cb0fec4143318e712970214e0d2011444
marcelo <marcelopc04@gmail.com>
2023-04-26 18:48:02 -0300

    about

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import styles from '@/styles/about.module.css'
+
+
+export default function About() {
+    return (
+        <div className={styles.about}>
+            <h1> Sobre o projeto </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem</p>
+        </div>
+    )
+
+}
